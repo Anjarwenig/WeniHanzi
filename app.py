98e2680f120d479591b5716f3c429851297e667e
Galuh Anjarweni <anjarwenig@gmail.com>
2025-07-04 00:47:09 -0700

    Update flashcard logic with back button and category

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, jsonify
 import json
-import os
 
 app = Flask(__name__)
 
-with open("mandarin_verbs_flashcard_200_detailed.json", encoding="utf-8") as f:
+with open("mandarin_flashcards_by_chapter.json", encoding="utf-8") as f:
     flashcards = json.load(f)
 
 @app.route("/")
@@ -16,5 +15,6 @@ def cards():
     return jsonify(flashcards)
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
