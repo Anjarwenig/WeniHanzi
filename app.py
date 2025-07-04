f3bd9b67b0156549e6d1002d87c2d4b496b08f53
Galuh Anjarweni <anjarwenig@gmail.com>
2025-07-04 00:07:31 -0700

    Fix deployment issue on Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def cards():
     return jsonify(flashcards)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
